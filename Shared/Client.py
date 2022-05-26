2847d3214b3bade598a6dffed6dca0beca613ebd
CodeLemons <asamalihamza@gmail.com>
2022-05-26 12:47:18 +0100

    Created a bare bones server and client using Twisted.

---
+++ Shared/Client.py
@@ -0,0 +1,6 @@
+from twisted.internet.protocol import Protocol
+from sys import stdout
+
+class Echo(Protocol):
+    def dataReceived(self, data):
+        stdout.write(data)
---
+++ Shared/Server.py
@@ -0,0 +1,5 @@
+from twisted.internet.protocol import Protocol
+
+class Echo(Protocol):
+    def dataReceived(self, data):
+        self.transport.write(data)
